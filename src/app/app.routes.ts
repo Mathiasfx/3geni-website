8fb20b58053d0d8719b0a68dafcf4cf7dccb1ebb
pi-mathiasdev <mathiaspereiradev@gmail.com>
2025-10-07 16:13:35 -0300

    add some components and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent }
+    ]
+  }
+];
+
