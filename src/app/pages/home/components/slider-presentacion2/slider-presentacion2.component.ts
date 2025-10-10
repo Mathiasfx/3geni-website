9485cde96e6ecdc9db071200d0870623159a7e00
pi-mathiasdev <mathiaspereiradev@gmail.com>
2025-10-10 01:56:31 -0300

    Add new secction

---
+++ src/app/pages/home/components/slider-presentacion2/slider-presentacion2.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-slider-presentacion2',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './slider-presentacion2.component.html',
+  styleUrl: './slider-presentacion2.component.scss'
+})
+export class SliderPresentacion2Component {
+  // Component logic can be added here if needed
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SliderPresentacion1Component } from './components/slider-presentacion1/slider-presentacion1.component';
+import { SliderPresentacion2Component } from "./components/slider-presentacion2/slider-presentacion2.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliderPresentacion1Component],
+  imports: [SliderPresentacion1Component, SliderPresentacion2Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
