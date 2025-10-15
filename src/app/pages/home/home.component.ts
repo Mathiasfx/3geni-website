c5c2eaab882b4be17ac83c0fa1aaaad9d290bc4e
pi-mathiasdev <mathiaspereiradev@gmail.com>
2025-10-15 10:34:51 -0300

    ADD: Add secction services in home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { SliderPresentacion1Component } from './components/slider-presentacion1/
 import { SliderPresentacion2Component } from "./components/slider-presentacion2/slider-presentacion2.component";
 import { SliderPresentacion3Component } from "./components/slider-presentacion3/slider-presentacion3.component";
 import { QuienesSomosComponent } from "./components/quienes-somos/quienes-somos.component";
+import { ServiciosComponent } from "./components/servicios/servicios.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliderPresentacion1Component, SliderPresentacion2Component, SliderPresentacion3Component, QuienesSomosComponent],
+  imports: [SliderPresentacion1Component, SliderPresentacion2Component, SliderPresentacion3Component, QuienesSomosComponent, ServiciosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
